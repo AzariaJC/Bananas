2ee891f8cf3bea9db838bdab33c1f3b3ac45b4cd
Azaria Chamorro <azariajc@gmail.com>
2018-12-23 14:53:55 -0500

    Maybe I'll get this right one day

---
+++ Bananas.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 14:12:47 2018
+
+@author: Azaria
+"""
+
+word = input("What is it?")
+
+i=0
+if word ==" it's bananas":
+    while i < len(word):
+        char = word[i]
+        print(char)
+        i+=1
+elif word !=" it's bananas":
+    print("incorrec") 
+print("to the tune of hollaback girl")
